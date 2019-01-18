b7d16a260dc40401f44ccaa5f389a8cc5c723491
zgm <zgm.developer@gmail.com>
2019-01-18 13:40:34 +0800

    change: statistic graph theme to macarons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { AppConfig } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {AppConfig} from './environments/environment';
+import 'echarts/theme/macarons.js';
 
 if (AppConfig.production) {
   enableProdMode();
